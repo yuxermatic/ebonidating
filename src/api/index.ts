64630af131e4296c40b12ca9b85416e3b3a3ebb4
yuxermatic <yuxermatick@gmail.com>
2025-10-11 11:45:26 +0000

    migrate to vercel typescript backend

---
+++ src/api/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to EboniDating API (Vercel Ready)" });
+});
+
+export default app;
